4a126858276b7bf09056eb51962b7789b99477bd
Ana Gonzalez <157073381+anagonzalez94@users.noreply.github.com>
2025-06-25 20:22:43 +0000

    Final commit

--- wee04.py
+++ wee04.py
@@ -1,4 +1,54 @@
+# Longest Word
+def longest_word(words: list[str]) -> str:
+  ## Examine the amount of characters (len) of each string in the array to determine the longest (max).
+  if not words:
+    return None # For null test, if "None" is input (because you cannot iterate nothing)
+  longest = max(words, key=len) # "key" is used here to parameterize
+  return str(longest)
 
+# Shortest Word
+def shortest_word(words: list[str]) -> str:
+  ## Similar to above, but now we are looking for the shortest string (min).
+  if not words:
+    return None # For null test
+  shortest = min(words, key=len)
+  return str(shortest)
+
+# Odd Words
+def odd_words(words: list[str]) -> list[str]:
+  if not words:
+    return None # For null test
+  odd_list = [] # A blank list to be filled with the odd strings found below
+  for word in words:
+    if len(word) % 2 != 0: # Determining if the length of each string is odd
+      odd_list.append(word) # Adds strings identified as odd to "odd_list".
+  return list[str](odd_list)
+
+# Average Words
+def average_words(words: list[str]) -> list[str]:
+  if not words:
+    return None
+ # Find the amount of characters in all strings ("map" iterates "len" for every value in our list) and divide by the total amount across all strings 
+  average: int = sum(map(len, words)) / len(words)
+  average_modified: int = average + 1
+  average_list = [] # A blank list to be filled with the average strings
+  for word in words:
+    if len(word) == average_modified: # Identifies if the length of each string matches the average plus one
+      average_list.append(word)
+  return list[str](average_list)
+
+# Intersection
+def intersect(foo: list[str], bar: list[str]) -> bool:
+  if not foo:
+    return None
+  if not bar:
+    return None
+# Iterate through all words in first list (foo) and determine if they appear in the second (bar)
+  shared = False 
+  for word in foo:
+    if word in bar:
+      shared = True
+  return shared
 
 #--------------------------------------------------------------------------------#
 # ⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎  WRITE YOUR CODE ABOVE THIS  LINE ⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎
---
+++ week03_reflection.md
@@ -0,0 +1,6 @@
+# Week 03 Reflection
+Regrettably, I submitted an incomplete assignment for last week. I consider my biggest mistake not giving myself enough time to work thoroughly, and will not repeat that! However, let's evaluate the code that I did write.
+## My code
+For the diamond drawing program, I did not distinguish between an odd and even height like the given solutions. I did have the same core algebraic idea however, just not proper execution. 
+For compound interest, I had the correct number of inputs in my header, however did not properly classify the principal or interest rate as float (instead, I had them as integers).
+Overall, I also did not include enough comments. Looking back at my code, I don't get enough of my thought process and it is clear that it was rushed. Going forward, I will make better use of my time in order to properly complete every assignment and not cheat myself out of learning opportunities. 
